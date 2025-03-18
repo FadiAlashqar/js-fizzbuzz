cb92f0e936a288a6a79f0316b3ae1c8a2d747aea
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-18 23:54:00 +0100

    [UPD] identidicazioni multipli di 5

--- js/script.js
+++ js/script.js
@@ -7,4 +7,8 @@ for (let i = 0; i <= 100; i++) {
     if (i % 3 == 0) {
         console.log("Fizz")
     }
+// assegno l'etichetta "Buzz" ai multipli di 5   
+    else if (i % 5 == 0) {
+        console.log("Buzz")
+    }
 }
