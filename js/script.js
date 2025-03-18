a62711db788a882b93d68ea694227aaf271bdad4
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-18 23:53:25 +0100

    [UPD] indetidicazioni multipli di 3

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// step 1: inizlizzasione variabile Contatore
+// step 2: condizione fine ciclo
+// step 3: incremento varibaile contatore
+
+for (let i = 0; i <= 100; i++) {
+// asssegno l'etichetta "Fizz" ai multipli di 3
+    if (i % 3 == 0) {
+        console.log("Fizz")
+    }
+}
