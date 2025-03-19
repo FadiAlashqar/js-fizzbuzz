2f8c391d7c89693e1291ff453573cfe3a1f67a0f
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-03-19 10:03:53 +0100

    [UPD] exercise cirrection done

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@
 // step 2: condizione fine ciclo
 // step 3: incremento varibaile contatore
 
-for (let i = 0; i <= 100; i++) {
-// asssegno l'etichetta "Fizz" ai multipli di 3
-    if (i % 3 == 0) {
-        console.log("Fizz")
+for (let i = 1; i <= 100; i++) {
+// assegno l'etichetta "FizzBuzz" ai multipli di 5 e 3
+    if (i % 5 == 0 && i % 3 == 0 ) {
+    console.log("FizzBuzz");
     }
 // assegno l'etichetta "Buzz" ai multipli di 5   
-    else if (i % 5 == 0) {
-        console.log("Buzz")
+    else if(i % 5 == 0) {
+        console.log("Buzz");
     }
-// assegno l'etichetta "FizzBuzz" ai multipli di 5 e 3
-else{
-    console.log("FizzBuzz")
+// asssegno l'etichetta "Fizz" ai multipli di 3
+    else if (i % 3 == 0) {
+    console.log("Fizz");
 }
-
+    else{
+        console.log(i);
+}
 }
